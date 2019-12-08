b369a3541a8a862e90f9b5ac8061fe420ecae3cf
Michael <michael.stillwell@hotmail.com>
2019-12-08 15:50:32 -0600

    Started Front End

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary" size="lg" active>
+        Test Button
+      </Button>
+      
+    </div>
+  );
+}
+
+export default App;
